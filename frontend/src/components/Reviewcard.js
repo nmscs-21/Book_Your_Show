73bb070fe7e41eb99bf32821001124a3ec2cf092
vineesh22 <vineeshguptau@gmail.com>
2024-04-11 21:05:24 +0530

    movie-details

---
+++ frontend/src/components/Reviewcard.js
@@ -0,0 +1,24 @@
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+const Reviewcard = ({ username, review }) => {
+  return (
+    <div
+      class="card"
+      style={{
+        flex: "0 0 auto",
+        width: "400px",
+        marginRight: "10px",
+        backgroundColor: "#ffffff",
+        padding: "10px",
+      }}
+    >
+      <div class="card-body">
+        <h4>{username}</h4>
+        <p>{review}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Reviewcard;
